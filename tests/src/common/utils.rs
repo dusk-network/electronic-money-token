3923c5d42bd2af9cf49617ae40fd20abba45d9f0
Neotamandua <107320179+Neotamandua@users.noreply.github.com>
2025-02-20 17:29:50 +0200

    Add mainnet session module

---
+++ tests/src/common/utils.rs
@@ -0,0 +1,37 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
+use std::sync::mpsc;
+
+use dusk_core::abi::ContractId;
+use dusk_core::signatures::bls::PublicKey as AccountPublicKey;
+use dusk_core::transfer::moonlight::AccountData;
+use dusk_core::transfer::phoenix::{Note, NoteLeaf, ViewKey as PhoenixViewKey};
+use dusk_core::transfer::TRANSFER_CONTRACT;
+use dusk_core::BlsScalar;
+use dusk_vm::{Error as VMError, Session};
+
+const GAS_LIMIT: u64 = 0x10_000_000;
+
+pub fn contract_balance(session: &mut Session, contract: ContractId) -> Result<u64, VMError> {
+    session
+        .call(TRANSFER_CONTRACT, "contract_balance", &contract, GAS_LIMIT)
+        .map(|r| r.data)
+}
+
+pub fn chain_id(session: &mut Session) -> Result<u8, VMError> {
+    session
+        .call(TRANSFER_CONTRACT, "chain_id", &(), GAS_LIMIT)
+        .map(|r| r.data)
+}
+
+// moonlight helper functions
+
+pub fn account(session: &mut Session, pk: &AccountPublicKey) -> Result<AccountData, VMError> {
+    session
+        .call(TRANSFER_CONTRACT, "account", pk, GAS_LIMIT)
+        .map(|r| r.data)
+}
