63f0227917e71b3193c7531f17b4bd5aa703c856
Neotamandua <107320179+Neotamandua@users.noreply.github.com>
2025-02-26 16:51:37 +0200

    types: adjust to contract changes
    - Move contract const DEFAULT_OWNER & SHIELDED_NOT_SUPPORTED into types

--- types/src/admin_management.rs
+++ types/src/admin_management.rs
@@ -5,60 +5,17 @@
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
 use bytecheck::CheckBytes;
+use dusk_core::abi::{ContractId, CONTRACT_ID_BYTES};
 use rkyv::{Archive, Deserialize, Serialize};
+
+use crate::Account;
 
 /// Error message for when the contract is paused.
 pub const PAUSED_MESSAGE: &str = "Contract is paused";
 
-/// Arguments for admin management transactions.
-pub mod arguments {
-    use dusk_core::signatures::bls::{SecretKey, Signature};
-
-    use super::*;
-
-    /// Data used to toggle the pause state of the contract.
-    #[derive(
-        Debug, Clone, Copy, PartialEq, Eq, Archive, Serialize, Deserialize,
-    )]
-    #[archive_attr(derive(CheckBytes))]
-    pub struct PauseToggle {
-        nonce: u64,
-        signature: Signature,
-    }
-
-    impl PauseToggle {
-        const SIGNATURE_MSG_SIZE: usize = 8;
-
-        /// Create a new pause toggle.
-        pub fn new(sender_sk: &SecretKey, nonce: u64) -> Self {
-            let mut toggle = Self {
-                nonce,
-                signature: Signature::default(),
-            };
-
-            let sig_msg = toggle.signature_message();
-            let sig = sender_sk.sign(&sig_msg);
-            toggle.signature = sig;
-
-            toggle
-        }
-
-        /// The nonce used for the toggle.
-        pub fn nonce(&self) -> u64 {
-            self.nonce
-        }
-
-        /// The signature used for the toggle.
-        pub fn signature(&self) -> &Signature {
-            &self.signature
-        }
-
-        /// The message to be signed over.
-        pub fn signature_message(&self) -> [u8; Self::SIGNATURE_MSG_SIZE] {
-            self.nonce.to_le_bytes()
-        }
-    }
-}
+/// Default owner i.e., no owner. This is the Zero address.
+pub const DEFAULT_OWNER: Account =
+    Account::Contract(ContractId::from_bytes([0; CONTRACT_ID_BYTES]));
 
 /// Events emitted by admin management transactions.
 pub mod events {
--- types/src/token.rs
+++ types/src/token.rs
@@ -9,7 +9,6 @@ pub mod account;
 pub use account::*;
 
 use bytecheck::CheckBytes;
-use dusk_core::signatures::bls::{PublicKey, SecretKey, Signature};
 use rkyv::{Archive, Deserialize, Serialize};
 
 /// Arguments to query for how much of an allowance a spender has of the `owner`
@@ -31,59 +30,17 @@ pub struct Allowance {
 )]
 #[archive_attr(derive(CheckBytes))]
 pub struct Transfer {
-    sender: Account,
     receiver: Account,
     value: u64,
-    nonce: u64,
-    /// The signature is optional as a contract account doesn't need to sign
-    /// the transfer.
-    signature: Signature,
 }
 
 impl Transfer {
-    const SIGNATURE_MSG_SIZE: usize = 194 + 194 + 8 + 8;
-
     /// Create a new transfer with an external account.
-    pub fn new_external(
-        sender_sk: &SecretKey,
-        sender: impl Into<Account>,
-        receiver: impl Into<Account>,
-        value: u64,
-        nonce: u64,
-    ) -> Self {
-        let mut transfer = Self {
-            sender: sender.into(),
-            receiver: receiver.into(),
-            value,
-            nonce,
-            signature: Signature::default(),
-        };
-
-        let sig_msg = transfer.signature_message();
-        let sig = sender_sk.sign(&sig_msg);
-        transfer.signature = sig;
-        transfer
-    }
-
-    /// Create a new transfer with a contract account.
-    pub fn new_contract(
-        sender: impl Into<Account>,
-        receiver: impl Into<Account>,
-        value: u64,
-        nonce: u64,
-    ) -> Self {
+    pub fn new(receiver: impl Into<Account>, value: u64) -> Self {
         Self {
-            sender: sender.into(),
             receiver: receiver.into(),
             value,
-            nonce,
-            signature: Signature::default(),
         }
-    }
-
-    /// The account to transfer from.
-    pub fn sender(&self) -> &Account {
-        &self.sender
     }
 
     /// The account to transfer to.
@@ -94,41 +51,6 @@ impl Transfer {
     /// The value to transfer.
     pub fn value(&self) -> u64 {
         self.value
-    }
-
-    /// The nonce used to sign the transfer.
-    pub fn nonce(&self) -> u64 {
-        self.nonce
-    }
-
-    /// The signature used for the transfer.
-    pub fn signature(&self) -> &Signature {
-        &self.signature
-    }
-
-    /// The message to be signed over.
-    pub fn signature_message(&self) -> [u8; Self::SIGNATURE_MSG_SIZE] {
-        let mut msg = [0u8; Self::SIGNATURE_MSG_SIZE];
-
-        let mut offset = 0;
-
-        let bytes = self.sender.to_bytes();
-        msg[offset..][..bytes.len()].copy_from_slice(&bytes);
-        offset += bytes.len();
-
-        let bytes = self.receiver.to_bytes();
-        msg[offset..][..bytes.len()].copy_from_slice(&bytes);
-        offset += bytes.len();
-
-        let bytes = self.value.to_le_bytes();
-        msg[offset..][..bytes.len()].copy_from_slice(&bytes);
-        offset += bytes.len();
-
-        let bytes = self.nonce.to_le_bytes();
-        msg[offset..][..bytes.len()].copy_from_slice(&bytes);
-        // offset += bytes.len();
-
-        msg
     }
 }
 
@@ -139,68 +61,27 @@ impl Transfer {
 )]
 #[archive_attr(derive(CheckBytes))]
 pub struct TransferFrom {
-    spender: Account,
     sender: Account,
     receiver: Account,
     value: u64,
-    nonce: u64,
-    signature: Signature,
 }
 
 impl TransferFrom {
-    const SIGNATURE_MSG_SIZE: usize = 194 + 194 + 194 + 8 + 8;
-
     /// Create a new transfer for external accounts, spending tokens from the
     /// `sender`.
-    pub fn new_external(
-        spender_sk: &SecretKey,
+    pub fn new(
         sender: impl Into<Account>,
         receiver: impl Into<Account>,
         value: u64,
-        nonce: u64,
-    ) -> Self {
-        let spender = PublicKey::from(spender_sk);
-
-        let mut transfer_from = Self {
-            spender: Account::External(spender),
-            sender: sender.into(),
-            receiver: receiver.into(),
-            value,
-            nonce,
-            signature: Signature::default(),
-        };
-
-        let sig_msg = transfer_from.signature_message();
-        let sig = spender_sk.sign(&sig_msg);
-        transfer_from.signature = sig;
-
-        transfer_from
-    }
-
-    /// Create a new transfer for contracts, spending tokens from the `sender`.
-    pub fn new_contract(
-        spender: impl Into<Account>,
-        sender: impl Into<Account>,
-        receiver: impl Into<Account>,
-        value: u64,
-        nonce: u64,
     ) -> Self {
         Self {
-            spender: spender.into(),
             sender: sender.into(),
             receiver: receiver.into(),
             value,
-            nonce,
-            signature: Signature::default(),
         }
     }
 
-    /// The account spending the tokens.
-    pub fn spender(&self) -> &Account {
-        &self.spender
-    }
-
-    /// The account that owns the tokens being transferred.
+    /// The account from which the tokens are spent.
     pub fn sender(&self) -> &Account {
         &self.sender
     }
@@ -214,45 +95,6 @@ impl TransferFrom {
     pub fn value(&self) -> u64 {
         self.value
     }
-
-    /// The nonce used to sign the transfer.
-    pub fn nonce(&self) -> u64 {
-        self.nonce
-    }
-
-    /// The signature used for the transfer.
-    pub fn signature(&self) -> &Signature {
-        &self.signature
-    }
-
-    /// The message to be signed over.
-    pub fn signature_message(&self) -> [u8; Self::SIGNATURE_MSG_SIZE] {
-        let mut msg = [0u8; Self::SIGNATURE_MSG_SIZE];
-
-        let mut offset = 0;
-
-        let bytes = self.spender.to_bytes();
-        msg[offset..][..bytes.len()].copy_from_slice(&bytes);
-        offset += bytes.len();
-
-        let bytes = self.sender.to_bytes();
-        msg[offset..][..bytes.len()].copy_from_slice(&bytes);
-        offset += bytes.len();
-
-        let bytes = self.receiver.to_bytes();
-        msg[offset..][..bytes.len()].copy_from_slice(&bytes);
-        offset += bytes.len();
-
-        let bytes = self.value.to_le_bytes();
-        msg[offset..][..bytes.len()].copy_from_slice(&bytes);
-        offset += bytes.len();
-
-        let bytes = self.nonce.to_le_bytes();
-        msg[offset..][..bytes.len()].copy_from_slice(&bytes);
-        // offset += bytes.len();
-
-        msg
-    }
 }
 
 /// Data used to approve spending tokens from a user's account.
@@ -261,59 +103,17 @@ impl TransferFrom {
 )]
 #[archive_attr(derive(CheckBytes))]
 pub struct Approve {
-    sender: Account,
     spender: Account,
     value: u64,
-    nonce: u64,
-    signature: Signature,
 }
 
 impl Approve {
-    const SIGNATURE_MSG_SIZE: usize = 194 + 194 + 8 + 8;
-
     /// Create a new approval for an external account.
-    pub fn new_external(
-        sender_sk: &SecretKey,
-        spender: impl Into<Account>,
-        value: u64,
-        nonce: u64,
-    ) -> Self {
-        let owner = PublicKey::from(sender_sk);
-
-        let mut approve = Self {
-            sender: Account::External(owner),
-            spender: spender.into(),
-            value,
-            nonce,
-            signature: Signature::default(),
-        };
-
-        let sig_msg = approve.signature_message();
-        let sig = sender_sk.sign(&sig_msg);
-        approve.signature = sig;
-
-        approve
-    }
-
-    /// Create a new approval for a contract account.
-    pub fn new_contract(
-        sender: impl Into<Account>,
-        spender: impl Into<Account>,
-        value: u64,
-        nonce: u64,
-    ) -> Self {
+    pub fn new(spender: impl Into<Account>, value: u64) -> Self {
         Self {
-            sender: sender.into(),
             spender: spender.into(),
             value,
-            nonce,
-            signature: Signature::default(),
         }
-    }
-
-    /// The account to allow the transfer of tokens.
-    pub fn sender(&self) -> &Account {
-        &self.sender
     }
 
     /// The account to allow spending tokens from.
@@ -324,41 +124,6 @@ impl Approve {
     /// The value to approve the transfer of.
     pub fn value(&self) -> u64 {
         self.value
-    }
-
-    /// The nonce used to sign the allowance.
-    pub fn nonce(&self) -> u64 {
-        self.nonce
-    }
-
-    /// The signature used for the allowance.
-    pub fn signature(&self) -> &Signature {
-        &self.signature
-    }
-
-    /// The message to be signed over.
-    pub fn signature_message(&self) -> [u8; Self::SIGNATURE_MSG_SIZE] {
-        let mut msg = [0u8; Self::SIGNATURE_MSG_SIZE];
-
-        let mut offset = 0;
-
-        let bytes = self.sender.to_bytes();
-        msg[offset..][..bytes.len()].copy_from_slice(&bytes);
-        offset += bytes.len();
-
-        let bytes = self.spender.to_bytes();
-        msg[offset..][..bytes.len()].copy_from_slice(&bytes);
-        offset += bytes.len();
-
-        let bytes = self.value.to_le_bytes();
-        msg[offset..][..bytes.len()].copy_from_slice(&bytes);
-        offset += bytes.len();
-
-        let bytes = self.nonce.to_le_bytes();
-        msg[offset..][..bytes.len()].copy_from_slice(&bytes);
-        // offset += bytes.len();
-
-        msg
     }
 }
 
