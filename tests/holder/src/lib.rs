10e384d8fa8bc73296c1db4e1908bb0cef8588ea
Neotamandua <107320179+Neotamandua@users.noreply.github.com>
2025-03-31 16:46:21 +0300

    tests: remove Transfer struct usage

--- tests/holder/src/lib.rs
+++ tests/holder/src/lib.rs
@@ -46,14 +46,14 @@ static mut STATE: TokenState = TokenState {
 impl TokenState {
     /// Can be called by anyone to make this contract send tokens to another
     /// account
-    fn token_send(&mut self, transfer: Transfer) {
+    fn token_send(&mut self, receiver: Account, value: u64) {
         if let Err(err) =
-            abi::call::<_, ()>(self.token_contract, "transfer", &transfer)
+            abi::call::<_, ()>(self.token_contract, "transfer", &(receiver, value))
         {
             panic!("Failed sending tokens: {err}");
         }
 
-        self.balance -= transfer.value();
+        self.balance -= value
     }
 
     /// Handles incoming token transfers from the token-contract.
@@ -79,7 +79,7 @@ unsafe fn init(arg_len: u32) -> u32 {
 
 #[no_mangle]
 unsafe fn token_send(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |arg| STATE.token_send(arg))
+    abi::wrap_call(arg_len, |(receiver, value)| STATE.token_send(receiver, value))
 }
 
 #[no_mangle]
