fc2a6238640df1fbc635407ff626cfc7f32fbd22
moana <moana@dusk.network>
2025-04-28 14:12:50 +0200

    allowlist: Add wasm export functions

--- allowlist/src/lib.rs
+++ allowlist/src/lib.rs
@@ -11,92 +11,63 @@
 #![deny(unused_extern_crates)]
 #![no_std]
 
-#[cfg(target_family = "wasm")]
-pub(crate) mod state;
-
-/*
-#[cfg(target_family = "wasm")]
-mod wasm {
+// #[cfg(target_family = "wasm")]
 extern crate alloc;
 
-use alloc::collections::BTreeMap;
-use alloc::string::String;
-use alloc::vec::Vec;
+// #[cfg(target_family = "wasm")]
+pub(crate) mod state;
+
+// #[cfg(target_family = "wasm")]
+// mod wasm {
 
 use dusk_core::abi;
-use dusk_core::transfer::data::ContractCall;
-use emt_core::token::error;
-use emt_core::token::events;
-use emt_core::{Account, AccountInfo, ZERO_ADDRESS};
+use emt_core::allowlist::Address;
+
+use crate::state::STATE;
 
 #[no_mangle]
 unsafe extern "C" fn init(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |(initial_accounts, ownership)| {
-        STATE.init(initial_accounts, ownership);
+    abi::wrap_call(arg_len, |(allowed, ownership)| {
+        STATE.init(allowed, ownership);
     })
-}
-
-#[no_mangle]
-unsafe extern "C" fn name(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |(): ()| TokenState::name())
-}
-
-#[no_mangle]
-unsafe extern "C" fn symbol(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |(): ()| TokenState::symbol())
-}
-
-#[no_mangle]
-unsafe extern "C" fn decimals(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |(): ()| TokenState::decimals())
-}
-
-#[no_mangle]
-unsafe extern "C" fn total_supply(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |(): ()| STATE.total_supply())
-}
-
-#[no_mangle]
-unsafe extern "C" fn account(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |arg| STATE.account(arg))
-}
-
-#[no_mangle]
-unsafe extern "C" fn balance_of(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |account| STATE.balance_of(account))
-}
-
-#[no_mangle]
-unsafe extern "C" fn allowance(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |(owner, spender)| STATE.allowance(owner, spender))
-}
-
-#[no_mangle]
-unsafe extern "C" fn transfer(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |(receiver, value)| STATE.transfer(receiver, value))
-}
-
-#[no_mangle]
-unsafe extern "C" fn transfer_and_call(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |(transfer, contract_call)| {
-        STATE.transfer_and_call(transfer, &contract_call);
-    })
-}
-
-#[no_mangle]
-unsafe extern "C" fn transfer_from(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |(owner, receiver, value)| {
-        STATE.transfer_from(owner, receiver, value);
-    })
-}
-
-#[no_mangle]
-unsafe extern "C" fn approve(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |(spender, value)| STATE.approve(spender, value))
 }
 
 /*
- * Access control functions
+ * Basic functionality of the allowlist-contract
+ */
+
+#[no_mangle]
+unsafe extern "C" fn is_allowed(arg_len: u32) -> u32 {
+    abi::wrap_call(arg_len, |user: Address| STATE.is_allowed(&user))
+}
+
+#[no_mangle]
+unsafe extern "C" fn has_role(arg_len: u32) -> u32 {
+    abi::wrap_call(arg_len, |user: Address| STATE.has_role(&user))
+}
+
+/*
+ * Functions only allowed to be executed by the registered ownership
+ * contract.
+ */
+
+#[no_mangle]
+unsafe extern "C" fn register(arg_len: u32) -> u32 {
+    abi::wrap_call(arg_len, |(user, role)| STATE.register(user, role))
+}
+
+#[no_mangle]
+unsafe extern "C" fn update(arg_len: u32) -> u32 {
+    abi::wrap_call(arg_len, |(user, role)| STATE.update(user, role))
+}
+
+#[no_mangle]
+unsafe extern "C" fn remove(arg_len: u32) -> u32 {
+    abi::wrap_call(arg_len, |user| STATE.remove(user))
+}
+
+/*
+ * Access control management functions
  */
 
 #[no_mangle]
@@ -115,114 +86,4 @@ unsafe extern "C" fn renounce_ownership(arg_len: u32) -> u32 {
 unsafe extern "C" fn ownership(arg_len: u32) -> u32 {
     abi::wrap_call(arg_len, |(): ()| STATE.ownership())
 }
-
-/*
- * Supply management functions
- */
-
-#[no_mangle]
-unsafe extern "C" fn mint(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |(receiver, amount)| STATE.mint(receiver, amount))
-}
-
-#[no_mangle]
-unsafe extern "C" fn burn(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |arg| STATE.burn(arg))
-}
-
-/*
- * Administrative functions
- */
-
-#[no_mangle]
-unsafe extern "C" fn toggle_pause(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |(): ()| STATE.toggle_pause())
-}
-
-#[no_mangle]
-unsafe extern "C" fn is_paused(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |(): ()| STATE.is_paused())
-}
-
-#[no_mangle]
-unsafe extern "C" fn force_transfer(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |(obliged_sender, receiver, value)| {
-        STATE.force_transfer(obliged_sender, receiver, value);
-    })
-}
-
-/*
- * Sanctions functions
- */
-
-#[no_mangle]
-unsafe extern "C" fn block(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |acc| STATE.block(acc))
-}
-
-#[no_mangle]
-unsafe extern "C" fn freeze(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |acc| STATE.freeze(acc))
-}
-
-#[no_mangle]
-unsafe extern "C" fn unblock(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |acc| STATE.unblock(acc))
-}
-
-#[no_mangle]
-unsafe extern "C" fn unfreeze(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |acc| STATE.unfreeze(acc))
-}
-
-#[no_mangle]
-unsafe extern "C" fn blocked(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |acc| STATE.blocked(acc))
-}
-
-#[no_mangle]
-unsafe extern "C" fn frozen(arg_len: u32) -> u32 {
-    abi::wrap_call(arg_len, |arg| STATE.frozen(arg))
-}
-}
-
-/*
- * Helper functions
- */
-
-/// Determines and returns the sender of the current transfer.
-///
-/// If the sender is an external account, return the transaction origin.
-/// If the sender is a contract, return the calling contract.
-///
-/// # Returns
-///
-/// An `Account` representing the token sender.
-///
-/// # Panics
-///
-/// - If no public sender is available (shielded transactions are not supported)
-/// - If no caller can be determined (impossible case)
-fn sender_account() -> Account {
-    // Identifies the sender by checking the call stack and transaction origin:
-    // - For direct external account transactions (call stack length = 1),
-    //   returns the transaction origin
-    // - For non-protocol contracts that call the token (call stack length > 1),
-    //   returns the immediate calling contract
-    // - This function panics for contract queries because their call stack
-    //   length is 0. It is not intended for "view" calls to use this function,
-    //   as they don't create traceable transactions with a sender/caller.
-    //   Instead, query functions should explicitly take information to query
-    //   for as an argument.
-    if abi::callstack().len() == 1 {
-        // This also implies, that the call directly originates via the protocol
-        // transfer contract i.e., the caller is the transfer
-        // contract
-        Account::External(
-            abi::public_sender().expect(error::SHIELDED_NOT_SUPPORTED),
-        )
-    } else {
-        Account::Contract(abi::caller().expect("ICC expects a caller"))
-    }
-}
-*/
+// }
